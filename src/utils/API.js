cdba5a705d0ec2a7121c3ab09103641e4eb184d8
Islam4049 <70667310+Islam4049@users.noreply.github.com>
2021-01-30 01:09:18 -0500

    commit all files

---
+++ src/utils/API.js
@@ -0,0 +1,8 @@
+import axios from "axios";
+// eslint-disable-next-line
+export default {
+    // Gets all users
+    getUsers: function () {
+        return axios.get("https://randomuser.me/api/?results=300&nat=us");
+    }
+};
