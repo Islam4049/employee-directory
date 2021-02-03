bae1c1643d1bb8b081db4f7e88356dd6ef9e341a
Islam4049 <70667310+Islam4049@users.noreply.github.com>
2021-02-03 01:28:13 -0500

    deployed

--- src/components/Container.js
+++ src/components/Container.js
@@ -1,8 +1,8 @@
 import API from "../utils/API";
-import React, { Component } from "react"
-import SearchBox from "./SearchBox"
-// import SearchBox from "./SearchBox"
-import TableData from "./TableData"
+import React, { Component } from "react";
+import SearchBox from "./SearchBox";
+//import SearchBox from "./SearchBox"
+import TableData from "./TableData";
 import "./style.css";
 
 class Container extends Component {
